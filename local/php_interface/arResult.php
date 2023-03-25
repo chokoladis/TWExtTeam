b24187748f12fbdfb1dc890444751ca6ec0ea265
chokoladis <igor2011__igor@mail.ru>
2023-03-25 23:27:05 +0400

    v0.2, 4.3hours

---
+++ local/php_interface/arResult.php
@@ -0,0 +1,237 @@
+2023-03-25 22:09:52 Запись в лог\n\n  ------ Array
+(
+    [ACTIVE] => Y
+    [MODIFIED_BY] => 1
+    [IBLOCK_ID] => 1
+    [ACTIVE_FROM] => 08.03.2023 12:00:00
+    [ACTIVE_TO] => 
+    [SORT] => 500
+    [NAME] => Новость1 с 8 марта!2822
+    [CODE] => news1
+    [TAGS] => 
+    [PREVIEW_PICTURE] => Array
+        (
+            [name] => 
+            [type] => 
+            [tmp_name] => 
+            [error] => 4
+            [size] => 0
+            [description] => 
+            [MODULE_ID] => iblock
+            [old_file] => 1
+        )
+
+    [PREVIEW_TEXT] => Текст превью
+    [PREVIEW_TEXT_TYPE] => text
+    [DETAIL_PICTURE] => Array
+        (
+            [name] => 
+            [type] => 
+            [tmp_name] => 
+            [error] => 4
+            [size] => 0
+            [description] => 
+            [MODULE_ID] => iblock
+            [old_file] => 2
+        )
+
+    [DETAIL_TEXT] => Подробная информация, детали и прочее
+    [DETAIL_TEXT_TYPE] => text
+    [TMP_ID] => 0
+    [PROPERTY_VALUES] => Array
+        (
+            [1] => Array
+                (
+                    [2] => Array
+                        (
+                            [VALUE] => Новость1 с 8 марта!2822
+                        )
+
+                )
+
+        )
+
+    [IBLOCK_SECTION] => Array
+        (
+        )
+
+    [IPROPERTY_TEMPLATES] => Array
+        (
+            [ELEMENT_META_TITLE] => 
+            [ELEMENT_META_KEYWORDS] => 
+            [ELEMENT_META_DESCRIPTION] => 
+            [ELEMENT_PAGE_TITLE] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_ALT] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_TITLE] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_NAME] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_ALT] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_TITLE] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_NAME] => 
+        )
+
+    [WF] => N
+    [WF_NEW] => 
+    [SEARCHABLE_CONTENT] => НОВОСТЬ1 С 8 МАРТА!2822
+ТЕКСТ ПРЕВЬЮ
+ПОДРОБНАЯ ИНФОРМАЦИЯ, ДЕТАЛИ И ПРОЧЕЕ
+    [ID] => 1
+)
+
+2023-03-25 22:10:32 Запись в лог\n\n  ------ Array
+(
+    [ACTIVE] => Y
+    [MODIFIED_BY] => 1
+    [IBLOCK_ID] => 1
+    [ACTIVE_FROM] => 08.03.2023 12:00:00
+    [ACTIVE_TO] => 
+    [SORT] => 500
+    [NAME] => Новость1 с 8 марта!282ff2
+    [CODE] => news1
+    [TAGS] => 
+    [PREVIEW_PICTURE] => Array
+        (
+            [name] => 
+            [type] => 
+            [tmp_name] => 
+            [error] => 4
+            [size] => 0
+            [description] => 
+            [MODULE_ID] => iblock
+            [old_file] => 1
+        )
+
+    [PREVIEW_TEXT] => dofeokf
+    [PREVIEW_TEXT_TYPE] => text
+    [DETAIL_PICTURE] => Array
+        (
+            [name] => 
+            [type] => 
+            [tmp_name] => 
+            [error] => 4
+            [size] => 0
+            [description] => 
+            [MODULE_ID] => iblock
+            [old_file] => 2
+        )
+
+    [DETAIL_TEXT] => Подробная информация, детали и прочее
+    [DETAIL_TEXT_TYPE] => text
+    [TMP_ID] => 0
+    [PROPERTY_VALUES] => Array
+        (
+            [1] => Array
+                (
+                    [2] => Array
+                        (
+                            [VALUE] => Новость1 с 8 марта!282ff2
+                        )
+
+                )
+
+        )
+
+    [IBLOCK_SECTION] => Array
+        (
+        )
+
+    [IPROPERTY_TEMPLATES] => Array
+        (
+            [ELEMENT_META_TITLE] => 
+            [ELEMENT_META_KEYWORDS] => 
+            [ELEMENT_META_DESCRIPTION] => 
+            [ELEMENT_PAGE_TITLE] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_ALT] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_TITLE] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_NAME] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_ALT] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_TITLE] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_NAME] => 
+        )
+
+    [WF] => N
+    [WF_NEW] => 
+    [SEARCHABLE_CONTENT] => НОВОСТЬ1 С 8 МАРТА!282FF2
+ТЕКСТ ПРЕВЬЮ
+ПОДРОБНАЯ ИНФОРМАЦИЯ, ДЕТАЛИ И ПРОЧЕЕ
+    [ID] => 1
+)
+
+2023-03-25 22:11:51 Запись в лог\n\n  ------ Array
+(
+    [ACTIVE] => Y
+    [MODIFIED_BY] => 1
+    [IBLOCK_ID] => 1
+    [ACTIVE_FROM] => 08.03.2023 12:00:00
+    [ACTIVE_TO] => 
+    [SORT] => 500
+    [NAME] => Новость1 с 8 марта!282ff266
+    [CODE] => news1
+    [TAGS] => 
+    [PREVIEW_PICTURE] => Array
+        (
+            [name] => 
+            [type] => 
+            [tmp_name] => 
+            [error] => 4
+            [size] => 0
+            [description] => 
+            [MODULE_ID] => iblock
+            [old_file] => 1
+        )
+
+    [PREVIEW_TEXT] => dofeokf
+    [PREVIEW_TEXT_TYPE] => text
+    [DETAIL_PICTURE] => Array
+        (
+            [name] => 
+            [type] => 
+            [tmp_name] => 
+            [error] => 4
+            [size] => 0
+            [description] => 
+            [MODULE_ID] => iblock
+            [old_file] => 2
+        )
+
+    [DETAIL_TEXT] => Подробная информация, детали и прочее
+    [DETAIL_TEXT_TYPE] => text
+    [TMP_ID] => 0
+    [PROPERTY_VALUES] => Array
+        (
+            [1] => Array
+                (
+                    [2] => Array
+                        (
+                            [VALUE] => Новость1 с 8 марта!282ff266
+                        )
+
+                )
+
+        )
+
+    [IBLOCK_SECTION] => Array
+        (
+        )
+
+    [IPROPERTY_TEMPLATES] => Array
+        (
+            [ELEMENT_META_TITLE] => 
+            [ELEMENT_META_KEYWORDS] => 
+            [ELEMENT_META_DESCRIPTION] => 
+            [ELEMENT_PAGE_TITLE] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_ALT] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_TITLE] => 
+            [ELEMENT_PREVIEW_PICTURE_FILE_NAME] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_ALT] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_TITLE] => 
+            [ELEMENT_DETAIL_PICTURE_FILE_NAME] => 
+        )
+
+    [WF] => N
+    [WF_NEW] => 
+    [SEARCHABLE_CONTENT] => НОВОСТЬ1 С 8 МАРТА!282FF266
+ТЕКСТ ПРЕВЬЮ
+ПОДРОБНАЯ ИНФОРМАЦИЯ, ДЕТАЛИ И ПРОЧЕЕ
+    [ID] => 1
+)
+
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,23 +1,80 @@
 <?
-AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", "UpdateNewsName");
 
-function UpdateNewsName($arFields){
+AddEventHandler("iblock", "OnStartIBlockElementUpdate", "StartUpdateNews");
 
-    $NAME = $arFields["NAME"];
+function StartUpdateNews(&$arFields){
 
-    mail(
-        $NAME
-    );
+    global $USER;
+    
+    if ($arFields["IBLOCK_ID"] == '1'){
 
-    CEvent::Send(
-        string event,
-        mixed lid,
-        array fields,
-        string duplicate="Y",
-        int message_id="",
-        array files,
-        string language_id
-    );
+        $file = 'arResult.php';
 
+        $res = CIBlockElement::GetProperty( $arFields["IBLOCK_ID"], $arFields["ID"] );
+        if ($ob = $res->GetNext())
+        {
+            if ($ob["CODE"] == 'OLD_NAME'){
+                if ($ob["VALUE"] == '' ){
 
+                    $arFields["PROPERTY_VALUES"][1][2]["VALUE"] = $arFields["NAME"];
+                } 
+            }
+        }
+        
+        // $log = date('Y-m-d H:i:s') . ' Запись в лог\n\n';
+        // $log .= ' '.' ------ ';
+        // $log .= print_r($arFields, true);
+        
+        // file_put_contents(__DIR__.'/arResult.php', $log . PHP_EOL, FILE_APPEND);
+
+        return $arFields;
+
+    }
+
+}
+
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", "BeforeUpdateNews");
+
+function BeforeUpdateNews(&$arFields){
+
+    global $USER;
+    
+    if ($arFields["IBLOCK_ID"] == '1'){
+
+        $file = 'arResult.php';
+
+        $res = CIBlockElement::GetProperty( $arFields["IBLOCK_ID"], $arFields["ID"] );
+        if ($ob = $res->GetNext())
+        {
+            if ($ob["CODE"] == 'OLD_NAME' && $ob["VALUE"] != $arFields["NAME"]){
+
+                $NAME = $arFields["NAME"];
+                $AUTHOR = $USER->GetLogin();
+        
+                $rsSites = CSite::GetByID("s1");
+                $arSite = $rsSites->Fetch();
+                $site_name = $arSite["NAME"];
+                $old_name = $arFields["PROPERTY_VALUES"][1][2]["VALUE"];
+            
+                CEvent::Send(
+                    "CHANGE_NEWS_NAME",
+                    's1',
+                    array(
+                        "SITE_NAME" => $site_name,
+                        "SERVER_NAME" => SITE_SERVER_NAME,
+                        "AUTHOR" => $AUTHOR,
+                        "NEWS_CODE" => $arFields["CODE"],
+                        "NEWS_NAME" => $arFields["NAME"],
+                        "NEWS_NAME_OLD" => $old_name
+                    ),
+                );
+
+                $arFields["PROPERTY_VALUES"][1][2]["VALUE"] = $arFields["NAME"];
+            } 
+        }
+    
+      
+    }
+
+    return $arFields;
 }
